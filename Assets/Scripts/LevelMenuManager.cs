881a1499c23bf8944d24666b66ab3d537f769f34
Shakamulia <satriacream45@gmail.com>
2025-04-16 22:37:34 +0700

    Save
    Save Sebelum Mulai Project

--- Assets/Scripts/LevelMenuManager.cs
+++ Assets/Scripts/LevelMenuManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelMenuManager : MonoBehaviour
@@ -7,12 +8,52 @@ public class LevelMenuManager : MonoBehaviour
 
     private void Start()
     {
-        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        PlayerPrefs.SetInt("Level1_Unlocked", 1); // Set the first level as unlocked by default
+        PlayerPrefs.Save(); // Save the PlayerPrefs
 
+        UpdateLevelButtons(); // Update the level buttons based on unlocked levels
+
+        // Load the unlocked level from PlayerPrefs
+        //int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+        //for (int i = 0; i < levelButtons.Length; i++)
+        //{
+        //    levelButtons[i].interactable = (i + 1) <= unlockedLevel;
+        //}
+    }
+
+
+    void UpdateLevelButtons()
+    {
         for (int i = 0; i < levelButtons.Length; i++)
         {
-            levelButtons[i].interactable = (i + 1) <= unlockedLevel;
+            int levelIndex = i + 1; // Level index starts from 1
+
+            bool isUnlocked = PlayerPrefs.GetInt("Level" + levelIndex + "_Unlocked", 0) == 1; // Check if the level is unlocked
+
+            //jika terkunci, nonaktifkan tombol dan beri warna abu abu
+            if (!isUnlocked)
+            {
+                levelButtons[i].interactable = false; // Disable the button
+                levelButtons[i].GetComponent<Image>().color = Color.gray; // Change button color to gray
+            }
+            else
+            {
+                levelButtons[i].onClick.AddListener(() => LoadLevel(levelIndex)); // Add listener to load the level
+            }
         }
+    }
+
+    void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene("Scene" + levelIndex); // Load the selected level
+    }
+
+    public static void UnlockNextLevel (int currentLevelIndex)
+    {
+        int nextLevel = currentLevelIndex + 1; // Unlock the next level
+        PlayerPrefs.SetInt("Level" + nextLevel + "_Unlocked", 1); // Set the next level as unlocked
+        PlayerPrefs.Save(); // Save the PlayerPrefs
     }
 
     public void ResetProgress()
---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerZone : MonoBehaviour
+{
+    public bool OneShot = false;
+    private bool AlreadyEntered = false; // To check if the player has already entered the trigger zone
+    private bool AlreadyExited = false; // To check if the player has already exited the trigger zone
+    public GameObject InteractionHint; // UI 'E' hint
+
+    public string collisionTag;
+
+    public UnityEvent OnTriggerEnter;
+    public UnityEvent OnTriggerExit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (AlreadyEntered)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(collisionTag) && !collision.CompareTag(collisionTag))
+        {
+            return;
+        }
+
+        OnTriggerEnter?.Invoke();
+
+        if(OneShot)
+            AlreadyEntered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (AlreadyExited)
+            return;
+
+        if (!string.IsNullOrEmpty(collisionTag) && !collision.CompareTag(collisionTag))
+        {
+            return;
+        }
+
+        OnTriggerExit?.Invoke();
+
+        if (OneShot)
+            AlreadyExited = true;
+    }
+    
+
+}
+
