b2be5ea08c5e35c2038126e73468438c72e3bd61
Shakamulia <satriacream45@gmail.com>
2025-04-17 11:04:35 +0700

    Soal Tambahan Beres.
    Mungkin Tinggal Rapikan lagi scene nya karena itu masih berantakan dan jika ada waktu bisa tambahkan vfx ketika hancur blokadenya

--- Assets/Scripts/DestructibleWall.cs
+++ Assets/Scripts/DestructibleWall.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
 public class DestructibleWall : MonoBehaviour {
+
+    [SerializeField] private GameObject destroyVFX;
+
     // jika masuk kedalam suatu collider maka dia akan ke trigger dan menghancurkan blokade
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Enemy")) {
             EnemyDash enemy = collision.gameObject.GetComponent<EnemyDash>();
             if (enemy != null && enemy.isDashing) {
+                Instantiate(destroyVFX, transform.position, Quaternion.identity);
                 Destroy(gameObject); // Hancurkan diri sendiri
             }
         }
